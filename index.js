159618a3c322fe7481dd8f9ba7b952b616dd1db7
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2024-11-16 21:57:46 -0600

    Modify createCanvas function to allow for dynamic resizing of the pixel count within the canvas element with a range slider

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function createCanvas(){
-// creates canvas grid of size 16 x 16 pixels
+function createCanvas(size){
+// creates canvas grid with dimensions size x size
     const canvas = document.createElement("div");
-    for(let i = 0; i < 16; i++){
-        for(let z = 0; z < 16; z++){
-            const pixel = document.createElement("div")
-            pixel.classList.add("pixel");
-            canvas.appendChild(pixel);
-        }
+    canvas.classList.add("canvas");
+    for(let i = 0; i < size * size; i++){
+        const pixel = document.createElement("div")
+        pixel.classList.add("pixel");
+        pixel.style.flexGrow = "1";
+        pixel.style.flexBasis = `calc(100% / ${size})`
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "black";
+        })
+        canvas.appendChild(pixel);
     }
 
     return canvas;
 }
 
 mainContent = document.querySelector("#main-content")
-const canvas = createCanvas();
-canvas.classList.add("canvas");
+let canvas = createCanvas(16);
+
+const canvasSizeRange = document.createElement('input');
+canvasSizeRange.type = 'range';
+canvasSizeRange.min = '1';
+canvasSizeRange.max = '16';
+canvasSizeRange.value = '16';
+
+canvasSizeRange.addEventListener("input", () => {
+    const newCanvas = createCanvas(canvasSizeRange.value);
+    canvas.replaceWith(newCanvas);
+    canvas = newCanvas;
+})
 
 mainContent.appendChild(canvas);
+mainContent.appendChild(canvasSizeRange);
